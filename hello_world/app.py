378323c37758d97cdc380c4a867eeb11d1b254a9
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-04-16 20:53:26 +0900

    up

---
+++ hello_world/app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI(docs_url='/docs',
+            #   openapi_url='/openapi.json',
+              openapi_prefix="/Prod"
+              )
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+lambda_handler = Mangum(app, lifespan="off")
