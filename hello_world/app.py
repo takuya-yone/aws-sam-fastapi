f91d10eab3057854a3f9b7fb670214862d56cc3b
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-04-18 00:12:56 +0900

    up

--- hello_world/app.py
+++ hello_world/app.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 from mangum import Mangum
 
 app = FastAPI(docs_url='/docs',
-            #   openapi_url='/openapi.json',
+              #   openapi_url='/openapi.json',
               openapi_prefix="/Prod"
               )
+
 
 @app.get("/")
 def read_root():
@@ -14,5 +15,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
 
 lambda_handler = Mangum(app, lifespan="off")
